81c89dec39d5ff71ec52ccb9c759e3221b9b2834
BreakerFeeder <wasem7915@gmail.com>
2021-10-30 10:05:32 +0200

    done

---
+++ app.js
@@ -0,0 +1,15 @@
+let text=document.getElementById("Typewriter");
+// // let text=document.querySelector("#Typewriter h2");
+const textArray =text.textContent.split(" ");
+// // console.log(textArray);
+let timeLooper;
+text.innerHTML="";
+function typewriterEffect(){
+    if(textArray.length > 0 ){
+        text.innerHTML +=textArray.shift()+"\n";
+    }else{
+        clearTimeout();
+    }
+    timeLooper =setTimeout('typewriterEffect()',Math.floor(Math.random()*(200,250)))
+}
+typewriterEffect();
